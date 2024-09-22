c68e2f7727376a0801702bd27c7953ae9ff3d7d7
Hannah Simon <147353617+hannahgsimon@users.noreply.github.com>
2024-09-22 01:28:37 -0400

    Update SortMethods.java

--- SortMethods.java
+++ SortMethods.java
@@ -1,10 +1,14 @@
-package sorting_algorithms;
-
+package project_sortingalgorithms;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Collections;
 import java.util.Arrays;
 import java.lang.Math;
+
+/**
+ *
+ * @author hannahgsimon
+ */
 
 public class SortMethods<T extends Comparable & SortProjectData>
 {
